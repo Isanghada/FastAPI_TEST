9e00f22f997dcb59e069bec2dd5a2f5ec9752116
susu4788 <susu4788@naver.com>
2023-02-24 16:58:58 +0900

    Feat : 추가 db

--- projects/myapi/main.py
+++ projects/myapi/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from domain.question import question_router
+
 app = FastAPI()
 
-origins=[
+origins = [
     "http://127.0.0.1:5173", "http://localhost:5173"
 ]
 
@@ -13,6 +15,10 @@ app.add_middleware(CORSMiddleware,
                    allow_methods=["*"],
                    allow_headers=["*"])
 
+
 @app.get("/hello")
 def hello():
-    return {"message":"안녕하세요 파이보"}
+    return {"message": "안녕하세요 파이보"}
+
+
+app.include_router(question_router.router)
